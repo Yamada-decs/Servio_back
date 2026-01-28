22f0519ea284febbdef357fd2be8245b495fbd8f
Yamada-decs <u22100700@utp.edu.pe>
2026-01-27 21:32:09 -0500

    Initial data

---
+++ src/main/java/com/servio/app/ServioApplication.java
@@ -0,0 +1,13 @@
+package com.servio.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servio/app/ServioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servio.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
